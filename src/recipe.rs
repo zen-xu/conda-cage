10bc582645e27f56e7d0ab0cbe7759a96119aff7
ZhengYu, Xu <zen-xu@outlook.com>
2022-07-29 23:29:55 +0800

    enable diff two recipes

--- src/recipe.rs
+++ src/recipe.rs
@@ -178,3 +178,177 @@ certifi                   2022.6.15        py37hecd8cb5_0    conda-forge
         }
     )
 }
+
+#[derive(Debug, Default, PartialEq)]
+pub struct RecipeDiff {
+    pub adds: Vec<Package>,
+    pub updates: Vec<Update>,
+    pub deletes: Vec<Package>,
+}
+
+impl RecipeDiff {
+    fn sort(&mut self) {
+        self.adds.sort_by(|a, b| a.name.cmp(&b.name));
+        self.updates.sort_by(|a, b| a.from.name.cmp(&b.from.name));
+        self.deletes.sort_by(|a, b| a.name.cmp(&b.name));
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Update {
+    pub from: Package,
+    pub to: Package,
+}
+
+impl Recipe {
+    fn diff(self, mut new_recipe: Self) -> RecipeDiff {
+        let mut diff = RecipeDiff::default();
+        for (pkg_name, old_pkg) in self.packages {
+            if let Some(new_pkg) = new_recipe.packages.remove(&pkg_name) {
+                if new_pkg != old_pkg {
+                    diff.updates.push(Update {
+                        from: old_pkg,
+                        to: new_pkg,
+                    })
+                }
+            } else {
+                diff.deletes.push(old_pkg)
+            }
+        }
+        diff.adds = new_recipe.packages.into_iter().map(|(_, v)| v).collect();
+
+        // sort
+        diff.sort();
+        diff
+    }
+}
+
+#[test]
+fn diff_two_recipe() {
+    use PackageKind::{Conda, PyPi};
+
+    let old_recipe: Recipe = r#"
+# Name                    Version                   Build  Channel
+aiohttp                   3.8.1                    pypi_0    pypi
+blas                      1.0                         mkl
+certifi                   2022.6.15        py37hecd8cb5_0    conda-forge
+frozenlist                1.3.0                    pypi_0    pypi
+multidict                 6.0.2                    pypi_0    pypi
+ncurses                   6.3                  hca72f7f_3
+numpy                     1.18.1           py37h7241aed_0
+yarl                      1.7.2                    pypi_0    pypi
+"#
+    .try_into()
+    .unwrap();
+    let new_recipe: Recipe = r#"
+# Name                    Version                   Build  Channel
+aiohttp                   3.8.2                    pypi_0    pypi
+blas                      1.2                         mkl
+idna                      3.3                      pypi_0    pypi
+libcxx                    12.0.0               h2f01273_0
+multidict                 6.0.2                    pypi_0    pypi
+ncurses                   6.3                  hca72f7f_3
+numpy                     1.18.2                   pypi_0    pypi
+yarl                      1.7.3                xaa72f7f_3    conda-forge
+"#
+    .try_into()
+    .unwrap();
+
+    let diff = old_recipe.diff(new_recipe);
+    let mut expected = RecipeDiff {
+        adds: vec![
+            Package {
+                name: "idna".into(),
+                version: "3.3".into(),
+                kind: PyPi,
+            },
+            Package {
+                name: "libcxx".into(),
+                version: "12.0.0".into(),
+                kind: Conda {
+                    build: "h2f01273_0".into(),
+                    channel: "default".into(),
+                },
+            },
+        ],
+        updates: vec![
+            Update {
+                from: Package {
+                    name: "numpy".into(),
+                    version: "1.18.1".into(),
+                    kind: Conda {
+                        build: "py37h7241aed_0".into(),
+                        channel: "default".into(),
+                    },
+                },
+                to: Package {
+                    name: "numpy".into(),
+                    version: "1.18.2".into(),
+                    kind: PyPi,
+                },
+            },
+            Update {
+                from: Package {
+                    name: "blas".into(),
+                    version: "1.0".into(),
+                    kind: Conda {
+                        build: "mkl".into(),
+                        channel: "default".into(),
+                    },
+                },
+                to: Package {
+                    name: "blas".into(),
+                    version: "1.2".into(),
+                    kind: Conda {
+                        build: "mkl".into(),
+                        channel: "default".into(),
+                    },
+                },
+            },
+            Update {
+                from: Package {
+                    name: "yarl".into(),
+                    version: "1.7.2".into(),
+                    kind: PyPi,
+                },
+                to: Package {
+                    name: "yarl".into(),
+                    version: "1.7.3".into(),
+                    kind: Conda {
+                        build: "xaa72f7f_3".into(),
+                        channel: "conda-forge".into(),
+                    },
+                },
+            },
+            Update {
+                from: Package {
+                    name: "aiohttp".into(),
+                    version: "3.8.1".into(),
+                    kind: PyPi,
+                },
+                to: Package {
+                    name: "aiohttp".into(),
+                    version: "3.8.2".into(),
+                    kind: PyPi,
+                },
+            },
+        ],
+        deletes: vec![
+            Package {
+                name: "frozenlist".into(),
+                version: "1.3.0".into(),
+                kind: PyPi,
+            },
+            Package {
+                name: "certifi".into(),
+                version: "2022.6.15".into(),
+                kind: Conda {
+                    build: "py37hecd8cb5_0".into(),
+                    channel: "conda-forge".into(),
+                },
+            },
+        ],
+    };
+    expected.sort();
+    assert_eq!(diff, expected);
+}
