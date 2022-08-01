4996630cbb94cd119cf8d199cb16666291ac516f
ZhengYu, Xu <zen-xu@outlook.com>
2022-08-01 13:51:13 +0800

    mv run_conda and spawn_conda into mod.rs

--- src/action/install.rs
+++ src/action/install.rs
@@ -1,17 +1,13 @@
-use std::{
-    collections::{HashMap, VecDeque},
-    ffi::OsStr,
-    process::Stdio,
-};
+use std::collections::{HashMap, VecDeque};
 
 use indicatif::{ProgressBar, ProgressStyle};
 use tokio::{
-    io::{AsyncBufReadExt, AsyncReadExt, BufReader},
-    process::{Child, Command},
+    io::{AsyncBufReadExt, BufReader},
     select, signal, spawn,
     sync::mpsc,
 };
 
+use super::{run_conda, spawn_conda};
 use crate::recipe::{Package, Recipe, RecipeDiff};
 
 pub async fn install(
@@ -368,38 +364,6 @@ struct CollectedPackages<'p> {
     conda_delete_pkgs: Vec<&'p Package>,
     pypi_install_pkgs: Vec<&'p Package>,
     pypi_delete_pkgs: Vec<&'p Package>,
-}
-
-/// this function will not block and return Child
-fn spawn_conda<I, S>(args: I) -> std::io::Result<Child>
-where
-    I: IntoIterator<Item = S>,
-    S: AsRef<OsStr>,
-{
-    Command::new("conda")
-        .args(args)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-}
-
-/// this function will block and return stdout when success
-async fn run_conda<I, S>(args: I) -> anyhow::Result<String>
-where
-    I: IntoIterator<Item = S>,
-    S: AsRef<OsStr>,
-{
-    let mut process = spawn_conda(args)?;
-    let mut msg = String::new();
-    if process.wait().await.unwrap().success() {
-        let mut stdout = process.stdout.unwrap();
-        let _ = stdout.read_to_string(&mut msg).await;
-        Ok(msg)
-    } else {
-        let mut stderr = process.stderr.unwrap();
-        let _ = stderr.read_to_string(&mut msg).await;
-        Err(anyhow::anyhow!(msg))
-    }
 }
 
 enum InstallEvent {
--- src/action/mod.rs
+++ src/action/mod.rs
@@ -1,3 +1,42 @@
 mod install;
 
 pub use install::install;
+
+use std::{ffi::OsStr, process::Stdio};
+
+use tokio::{
+    io::AsyncReadExt,
+    process::{Child, Command},
+};
+
+/// this function will not block and return Child
+fn spawn_conda<I, S>(args: I) -> std::io::Result<Child>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    Command::new("conda")
+        .args(args)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+}
+
+/// this function will block and return stdout when success
+async fn run_conda<I, S>(args: I) -> anyhow::Result<String>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let mut process = spawn_conda(args)?;
+    let mut msg = String::new();
+    if process.wait().await.unwrap().success() {
+        let mut stdout = process.stdout.unwrap();
+        let _ = stdout.read_to_string(&mut msg).await;
+        Ok(msg)
+    } else {
+        let mut stderr = process.stderr.unwrap();
+        let _ = stderr.read_to_string(&mut msg).await;
+        Err(anyhow::anyhow!(msg))
+    }
+}
